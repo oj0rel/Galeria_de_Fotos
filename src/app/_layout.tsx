d894e7c9c8db64e69dd300eb5399c9b6ec3b3696
oj0rel <ogabrieusiuva@gmail.com>
2025-11-10 16:35:46 -0300

    Adição das fonts personalizadas.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,27 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+
+  const [fontsLoaded, fontError] = useFonts({
+    'GeistMono-Regular': require('../assets/fonts/GeistMono-Regular.ttf'),
+    'GeistMono-SemiBold': require('../assets/fonts/GeistMono-SemiBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <Stack>
